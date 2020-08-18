5c1b3ef35f02cebddd877438a65c81b468b77ca9
afiffauz1 <mz.afif28@gmail.com>
2020-08-18 09:39:54 +0700

    fix minor bug

--- src/scripts/data/data-source.js
+++ src/scripts/data/data-source.js
@@ -1,27 +1,27 @@
 class DataSource {
-    static cariFilm(keyword) {
-        return fetch(`http://www.omdbapi.com/?apikey=d386a0ab&s=${keyword}`)
-            .then(response => response.json())
-            .then(responseJson => {
-                if (responseJson.Search) {
-                    return Promise.resolve(responseJson.Search)
-                } else {
-                    return Promise.reject("Film Tidak Ditemukan!!")
-                }
-            })
-    }
+  static cariFilm(keyword) {
+    return fetch(`https://www.omdbapi.com/?apikey=d386a0ab&s=${keyword}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson.Search) {
+          return Promise.resolve(responseJson.Search);
+        } else {
+          return Promise.reject("Film Tidak Ditemukan!!");
+        }
+      });
+  }
 
-    static movieDetail(movieId) {
-        return fetch(`http://www.omdbapi.com/?apikey=d386a0ab&i=${movieId}`)
-            .then(response => response.json())
-            .then(responseJson => {
-                if (responseJson) {
-                    return Promise.resolve(responseJson)
-                } else {
-                    return Promise.reject("Film Tidak Ditemukan!!")
-                }
-            })
-    }
+  static movieDetail(movieId) {
+    return fetch(`https://www.omdbapi.com/?apikey=d386a0ab&i=${movieId}`)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        if (responseJson) {
+          return Promise.resolve(responseJson);
+        } else {
+          return Promise.reject("Film Tidak Ditemukan!!");
+        }
+      });
+  }
 }
 
 export default DataSource;
